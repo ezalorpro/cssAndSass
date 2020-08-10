e82bf6d08416f2b71f1a482d0d2cfc8c86e08dd2
kleiver <kleiver615@gmail.com>
2020-08-10 10:52:11 -0400

    transiciones boletas

---
+++ html/wordpress.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container-basico">
+    <label for="input1" class="input-label"> Probando html basico</label>
+    <input type="text" name="entrada" id="input1" class="input-class">
+</div>
+</template>
+
+<style>
+    .container-basico {
+        display: flex;
+        flex-flow: column;
+        width: 100%;
+
+        
+    }
+
+    .container-basico > .input-label {
+        margin-left: 50px;
+        font-style: italic;
+        color: #667722;
+        transition: 0.3s all ease;
+
+    }
+
+    .container-basico > .input-label:hover {
+        transform: scale(1.05);
+        font-style: italic;
+        color: #4a5718;
+               
+    }
+</style>
